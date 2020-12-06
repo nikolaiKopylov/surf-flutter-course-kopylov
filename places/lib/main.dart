c3bcc249f56e2329fde8b4074920065b3fd2048e
nikolaiKopylov <nikolai.kopylov89@gmail.com>
2020-12-06 22:44:42 +0300

    dev(MatApp/context) create App and getContextRuntime()

--- places/lib/main.dart
+++ places/lib/main.dart
@@ -1,68 +1,15 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(App());
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-          visualDensity: VisualDensity.adaptivePlatformDensity,
-        ),
-        home: MyFirstFulWidget()
-        // MyHomePage(title: 'Flutter Demo Home Page'),
-        );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      home: MyFirstFulWidget(),
+      title: 'FirstApp',
     );
   }
 }
@@ -79,6 +26,8 @@ class MyFirstWidget extends StatelessWidget {
       ),
     );
   }
+  // не сработало так как, context не является переменой сласса и находится вне зоны видимости
+  //Type getContextRuntime() => context.runtimeType;
 }
 
 class MyFirstFulWidget extends StatefulWidget {
@@ -92,6 +41,8 @@ class _MyFirstFulWidgetState extends State<MyFirstFulWidget> {
   Widget build(BuildContext context) {
     _callBuild++;
     print('Build StatefulWidget call - $_callBuild');
+    Type getContextRuntime() => context.runtimeType;
+    print(getContextRuntime());
     return Container(
         child: Center(
       child: Text('Hello'),
