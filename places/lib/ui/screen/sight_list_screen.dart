4e2ae4e8211b42e7e540dfc3572c0f4463994b5b
nikolaiKopylov <nikolai.kopylov89@gmail.com>
2020-12-08 22:35:36 +0300

    dev(text/textStyle): add AppBar, text, textstyle

--- places/lib/ui/screen/sight_list_screen.dart
+++ places/lib/ui/screen/sight_list_screen.dart
@@ -10,7 +10,15 @@ class _SightListScreenState extends State<SightListScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("App Bar Scaffold"),
+        backgroundColor: Colors.white,
+        elevation: 0.0,
+        toolbarHeight: 72.0,
+        title: Text(
+          "Список \nинтересных мест",
+          style: TextStyle(fontFamily: 'Roboto', color: Colors.black),
+          textAlign: TextAlign.start,
+          maxLines: 2,
+        ),
       ),
     );
   }
