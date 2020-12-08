5906c3cb7a73f49dc34a2ab6197704c5004e2a83
nikolaiKopylov <nikolai.kopylov89@gmail.com>
2020-12-08 23:35:01 +0300

    dev(RichText): AppBar RichText

--- places/lib/ui/screen/sight_list_screen.dart
+++ places/lib/ui/screen/sight_list_screen.dart
@@ -13,13 +13,41 @@ class _SightListScreenState extends State<SightListScreen> {
         backgroundColor: Colors.white,
         elevation: 0.0,
         toolbarHeight: 72.0,
-        title: Text(
-          "Список \nинтересных мест",
-          style: TextStyle(
-              fontFamily: 'Roboto', color: Colors.black, fontSize: 32),
-          textAlign: TextAlign.start,
-          maxLines: 2,
-        ),
+        title: RichText(
+            text: TextSpan(children: [
+          TextSpan(
+              text: 'С',
+              style: TextStyle(
+                  fontFamily: 'Roboto',
+                  color: Colors.green,
+                  fontSize: 32,
+                  fontWeight: FontWeight.bold),
+              children: [
+                TextSpan(
+                    text: 'писок',
+                    style: TextStyle(
+                        fontFamily: 'Roboto',
+                        color: Colors.black,
+                        fontSize: 32,
+                        fontWeight: FontWeight.bold))
+              ]),
+          TextSpan(
+              text: '\nи',
+              style: TextStyle(
+                  fontFamily: 'Roboto',
+                  color: Colors.yellow,
+                  fontSize: 32,
+                  fontWeight: FontWeight.bold),
+              children: [
+                TextSpan(
+                    text: 'нтересных мест',
+                    style: TextStyle(
+                        fontFamily: 'Roboto',
+                        color: Colors.black,
+                        fontSize: 32,
+                        fontWeight: FontWeight.bold)),
+              ])
+        ])),
       ),
     );
   }
