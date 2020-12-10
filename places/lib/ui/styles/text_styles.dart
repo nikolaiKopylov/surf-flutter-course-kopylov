7cd6b8710e02d244c07679aef1edc9c131e9f5e8
nikolaiKopylov <nikolai.kopylov89@gmail.com>
2020-12-10 22:58:43 +0300

    dev(textStyles):  create file lib/ui/styles/text_styles.dart
     Changes to be committed:
    	modified:   places/lib/ui/screen/sight_list_screen.dart
    	new file:   places/lib/ui/styles/text_styles.dart

---
+++ places/lib/ui/styles/text_styles.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class AppTextStyles {
+  static const textStyleAppBarBlack = TextStyle(
+    fontFamily: 'Roboto',
+    color: Colors.black,
+    fontSize: 32,
+    fontWeight: FontWeight.bold,
+  );
+
+  static const textStyleAppBarYellow = TextStyle(
+    fontFamily: 'Roboto',
+    color: Colors.yellow,
+    fontSize: 32,
+    fontWeight: FontWeight.bold,
+  );
+
+  static const textStyleAppBarGreen = TextStyle(
+    fontFamily: 'Roboto',
+    color: Colors.green,
+    fontSize: 32,
+    fontWeight: FontWeight.bold,
+  );
+}
