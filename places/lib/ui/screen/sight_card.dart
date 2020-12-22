ba7c316b4199ce1bd3868369fb4b720b0620762e
nikolaiKopylov <nikolai.kopylov89@gmail.com>
2020-12-22 21:41:11 +0300

    dev(AspectRatio): приведение SightCard к 3/2
     Changes to be committed:
    	modified:   places/lib/ui/screen/sight_card.dart

--- places/lib/ui/screen/sight_card.dart
+++ places/lib/ui/screen/sight_card.dart
@@ -16,70 +16,73 @@ class SightCard extends StatelessWidget {
   }) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.only(
-        left: 16,
-        top: 16,
-        right: 16,
-      ),
-      child: Column(
-        children: [
-          Stack(
-            children: [
-              Container(
-                color: Colors.amber,
-                width: double.infinity,
-                height: 96,
-              ),
-              Positioned(
-                left: 16,
-                top: 16,
-                child: Text(sight.type),
-              ),
-              Positioned(
-                right: 16,
-                top: 16,
-                child: Container(
-                  color: Colors.red,
-                  width: 20,
-                  height: 18,
-                ),
-              ),
-            ],
-          ),
-          Container(
-            padding: EdgeInsets.only(
-              right: 16,
-              left: 16,
-            ),
-            height: 92,
-            width: double.infinity,
-            color: AppColors.greyBackground,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
+    return AspectRatio(
+      aspectRatio: 3.0 / 2.0,
+      child: Padding(
+        padding: EdgeInsets.only(
+          left: 16,
+          top: 16,
+          right: 16,
+        ),
+        child: Column(
+          children: [
+            Stack(
               children: [
-                SizedBox(
-                  height: 16,
-                ),
-                ConstrainedBox(
-                  constraints: BoxConstraints(maxWidth: 151, maxHeight: 62),
-                  child: Text(
-                    sight.name,
-                    style: AppTextStyles.textStyleSightCardBottomUp,
-                    textAlign: TextAlign.left,
-                  ),
-                ),
                 Container(
-                  alignment: Alignment.topLeft,
-                  child: Text(
-                    AppTexts.workTime,
-                    style: AppTextStyles.textStyleSightCardBottomDown,
+                  color: Colors.amber,
+                  width: double.infinity,
+                  height: 96,
+                ),
+                Positioned(
+                  left: 16,
+                  top: 16,
+                  child: Text(sight.type),
+                ),
+                Positioned(
+                  right: 16,
+                  top: 16,
+                  child: Container(
+                    color: Colors.red,
+                    width: 20,
+                    height: 18,
                   ),
                 ),
               ],
             ),
-          ),
-        ],
+            Container(
+              padding: EdgeInsets.only(
+                right: 16,
+                left: 16,
+              ),
+              height: 92,
+              width: double.infinity,
+              color: AppColors.greyBackground,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(
+                    height: 16,
+                  ),
+                  ConstrainedBox(
+                    constraints: BoxConstraints(maxWidth: 151, maxHeight: 62),
+                    child: Text(
+                      sight.name,
+                      style: AppTextStyles.textStyleSightCardBottomUp,
+                      textAlign: TextAlign.left,
+                    ),
+                  ),
+                  Container(
+                    alignment: Alignment.topLeft,
+                    child: Text(
+                      AppTexts.workTime,
+                      style: AppTextStyles.textStyleSightCardBottomDown,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
