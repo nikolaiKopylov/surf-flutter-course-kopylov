0ec81447eb5f4ce6d88df69c13a7efd4bf3f6035
nikolaiKopylov <nikolai.kopylov89@gmail.com>
2021-01-13 23:17:10 +0300

    dev(visiting_screen): add VisitingScreen, SightCardWantVisit, SightCardVisited
     On branch 4-6-tabbar
     Changes to be committed:
    	new file:   places/lib/ui/screen/sight_card_visited.dart
    	new file:   places/lib/ui/screen/sight_card_want_visit.dart
    	new file:   places/lib/ui/screen/visiting_screen.dart

---
+++ places/lib/ui/screen/sight_card_want_visit.dart
@@ -0,0 +1,105 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:places/domain/sight.dart';
+import 'package:places/ui/constants.dart';
+import 'package:places/ui/styles/text_styles.dart';
+import 'package:places/ui/widget/image_network.dart';
+
+/// Экран карточки места посещение которого запланнировано
+class SightCardWantVisit extends StatelessWidget {
+  final Sight sight;
+
+  const SightCardWantVisit({
+    Key key,
+    @required this.sight,
+  }) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return AspectRatio(
+      aspectRatio: 3.0 / 2.0,
+      child: Padding(
+        padding: EdgeInsets.only(
+          left: 16,
+          top: 16,
+          right: 16,
+        ),
+        child: Column(
+          children: [
+            Stack(
+              children: [
+                Container(
+                  width: double.infinity,
+                  height: 96,
+                  child: ImageNetwork(imageUrl: sight.url),
+                ),
+                Positioned(
+                  left: 16,
+                  top: 16,
+                  child: Text(sight.type),
+                ),
+                Positioned(
+                  right: 16,
+                  top: 16,
+                  child: Container(
+                    width: 20,
+                    height: 18,
+                    child: Image.asset('res/icon/icon_close.png'),
+                  ),
+                ),
+                Positioned(
+                  right: 56,
+                  top: 16,
+                  child: Container(
+                    width: 20,
+                    height: 18,
+                    child: Image.asset('res/icon/icon_calendar_white.png'),
+                  ),
+                ),
+              ],
+            ),
+            Container(
+              padding: EdgeInsets.only(
+                right: 16,
+                left: 16,
+                bottom: 16,
+              ),
+              height: 92,
+              width: double.infinity,
+              color: AppColors.greyBackground,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(
+                    height: 16,
+                  ),
+                  ConstrainedBox(
+                    constraints: BoxConstraints(maxWidth: 160, maxHeight: 62),
+                    child: Text(
+                      sight.name,
+                      style: AppTextStyles.textStyleSightCardBottomUp,
+                      textAlign: TextAlign.left,
+                    ),
+                  ),
+                  Container(
+                    alignment: Alignment.topLeft,
+                    child: Text(
+                      AppTexts.wantVisitTime,
+                      style: AppTextStyles.textStyleWantVisitTime,
+                    ),
+                  ),
+                  Container(
+                    alignment: Alignment.topLeft,
+                    child: Text(
+                      AppTexts.workTime,
+                      style: AppTextStyles.textStyleSightCardBottomDown,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ places/lib/ui/screen/visiting_screen.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:places/ui/constants.dart';
+import 'package:places/ui/styles/text_styles.dart';
+
+import '../../mocks.dart';
+import 'sight_card_visited.dart';
+import 'sight_card_want_visit.dart';
+
+class VisitingScreen extends StatefulWidget {
+  @override
+  _VisitingScreenState createState() => _VisitingScreenState();
+}
+
+class _VisitingScreenState extends State<VisitingScreen> {
+  int tabIndex;
+  Color tabColor;
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 2,
+      initialIndex: 0,
+      child: Scaffold(
+        appBar: AppBar(
+          toolbarHeight: 108.0,
+          centerTitle: true,
+          title: Text(
+            AppTexts.favorites,
+            style: AppTextStyles.textStyleDetailTitle,
+          ),
+          backgroundColor: AppColors.greyBackground,
+          bottom: TabBar(
+            unselectedLabelColor: AppColors.grey2,
+            labelColor: AppColors.white,
+            indicator: BoxDecoration(
+              borderRadius: BorderRadius.circular(40),
+              color: AppColors.black,
+            ),
+            tabs: [
+              Tab(
+                child: Text(
+                  AppTexts.wantVisit,
+                ),
+              ),
+              Tab(
+                child: Text(
+                  AppTexts.visited,
+                ),
+              ),
+            ],
+          ),
+        ),
+        body: TabBarView(
+          children: [
+            SingleChildScrollView(
+              child: Column(
+                children: mocks
+                    .map((item) => SightCardWantVisit(
+                          sight: item,
+                        ))
+                    .toList(),
+              ),
+            ),
+            SingleChildScrollView(
+              child: Column(
+                children: mocks
+                    .map((item) => SightCardVisited(
+                          sight: item,
+                        ))
+                    .toList(),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
