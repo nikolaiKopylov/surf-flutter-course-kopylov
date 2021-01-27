2c4068529d549de1880034b8e8c05cabd346a7f8
nikolaiKopylov <nikolai.kopylov89@gmail.com>
2021-01-27 07:44:18 +0300

    dev(main): add themes to app

--- places/lib/main.dart
+++ places/lib/main.dart
@@ -3,6 +3,7 @@ import 'package:places/mocks.dart';
 import 'package:places/ui/screen/sight_list_screen.dart';
 import 'ui/screen/sight_detail.dart';
 import 'ui/screen/visiting_screen.dart';
+import 'ui/screen/res/themes.dart';
 
 void main() {
   runApp(App());
@@ -12,12 +13,12 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: VisitingScreen(),
-      //  SightListScreen(), //
-      //    SightDetail(
-      //  sight: mocks[0],
-      //),
-
+      theme: lightTheme,
+      home: //VisitingScreen(),
+          //SightListScreen(), //
+          SightDetail(
+        sight: mocks[0],
+      ),
       title: 'FirstApp',
     );
   }
