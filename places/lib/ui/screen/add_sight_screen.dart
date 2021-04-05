b1fc1e032efddf8967664ecb6ce269e400949f57
nikolaiKopylov <nikolai.kopylov89@gmail.com>
2021-04-05 21:57:07 +0300

    dev(add_sight_screen): Изменено создание полей ввода
    Вместо нескольких полей создан отдельный виджет для ввода данных
    Widget _addField({
        TextEditingController controller,
        FocusNode currentFocus,
        FocusNode nextFocus,
        String hint,
        bool multipleLines,
        bool lastField = false,
        BuildContext context,
        String title,
        bool numericField = false,
        String nameField,
      })
    
     Changes to be committed:
    	modified:   lib/ui/screen/add_sight_screen.dart

--- places/lib/ui/screen/add_sight_screen.dart
+++ places/lib/ui/screen/add_sight_screen.dart
@@ -1,24 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:places/domain/sight.dart';
+import 'package:places/mocks.dart';
 import 'package:places/ui/constants.dart';
 import 'package:places/ui/screen/res/themes.dart';
 
+/// AddSightScreen - экран создания нового места [Sight]
+/// и добавлния его в массив моковых даных[mocks]
 class AddSightScreen extends StatefulWidget {
   @override
   _AddSightScreenState createState() => _AddSightScreenState();
 }
 
 class _AddSightScreenState extends State<AddSightScreen> {
+  final TextEditingController nameController = TextEditingController();
+  final TextEditingController descriptionController = TextEditingController();
+  final TextEditingController latController = TextEditingController();
+  final TextEditingController lonController = TextEditingController();
+
+  final FocusNode nameFocusNode = FocusNode();
+  final FocusNode descriptionFocusNode = FocusNode();
+  final FocusNode latFocusNode = FocusNode();
+  final FocusNode lonFocusNode = FocusNode();
+
+  Map _valuesAdd = {};
+  bool _check = false;
+
   @override
   Widget build(BuildContext context) {
-    final TextEditingController nameController = TextEditingController();
-    final TextEditingController descriptionController = TextEditingController();
-    final TextEditingController latController = TextEditingController();
-    final TextEditingController lonController = TextEditingController();
-    final FocusNode nameFocusNode = FocusNode();
-    final FocusNode descriptionFocusNode = FocusNode();
-    final FocusNode latFocusNode = FocusNode();
-    final FocusNode lonFocusNode = FocusNode();
-
     return Scaffold(
       appBar: AppBar(
         toolbarHeight: 56,
@@ -56,6 +65,7 @@ class _AddSightScreenState extends State<AddSightScreen> {
                 lastField: false,
                 multipleLines: false,
                 title: AppTexts.name,
+                nameField: 'name',
               ),
               Row(
                 children: [
@@ -70,6 +80,7 @@ class _AddSightScreenState extends State<AddSightScreen> {
                       multipleLines: false,
                       title: AppTexts.latitude,
                       numericField: true,
+                      nameField: 'lat',
                     ),
                   ),
                   SizedBox(
@@ -86,6 +97,7 @@ class _AddSightScreenState extends State<AddSightScreen> {
                       multipleLines: false,
                       title: AppTexts.longitude,
                       numericField: true,
+                      nameField: 'lon',
                     ),
                   ),
                 ],
@@ -104,6 +116,7 @@ class _AddSightScreenState extends State<AddSightScreen> {
                 lastField: true,
                 multipleLines: true,
                 title: AppTexts.descriotion,
+                nameField: 'details',
               ),
               SizedBox(
                 height: 116,
@@ -120,7 +133,12 @@ class _AddSightScreenState extends State<AddSightScreen> {
                 shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(12),
                 ),
-                onPressed: () {},
+                onPressed: _check
+                    ? () {
+                        print(_valuesAdd);
+                        addNewSight();
+                      }
+                    : null,
                 child: Text(AppTexts.create.toUpperCase(),
                     style: Theme.of(context).textTheme.caption),
               ),
@@ -130,8 +148,151 @@ class _AddSightScreenState extends State<AddSightScreen> {
       ),
     );
   }
+
+  /// _addField - виджет создания  TextField для ввода данных нового места
+  ///  кроме выбора категории(описано отдельным виджетом)
+  Widget _addField({
+    TextEditingController controller,
+    FocusNode currentFocus,
+    FocusNode nextFocus,
+    String hint,
+    bool multipleLines,
+    bool lastField = false,
+    BuildContext context,
+    String title,
+    bool numericField = false,
+    String nameField,
+  }) {
+    assert(controller != null);
+    assert(currentFocus != null);
+    assert(hint != null);
+    assert(multipleLines != null);
+    assert(context != null);
+    assert(title != null);
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Container(
+          padding: EdgeInsets.only(
+            top: 24.0,
+            bottom: 12,
+          ),
+          child: Text(title.toUpperCase()),
+        ),
+        TextField(
+          minLines: multipleLines ? 2 : null,
+          maxLines: multipleLines ? 2 : null,
+          controller: controller,
+          focusNode: currentFocus,
+          autofocus: true,
+          keyboardType: numericField ? TextInputType.number : null,
+          textInputAction: TextInputAction.go,
+          onChanged: (value) {
+            _valuesAdd[nameField] = value;
+            checkAddValues();
+            print(_valuesAdd);
+          },
+          onEditingComplete: () {
+            lastField
+                ? FocusScope.of(context).unfocus()
+                : FocusScope.of(context).requestFocus(nextFocus);
+          },
+          decoration: InputDecoration(
+            contentPadding: EdgeInsets.symmetric(
+              vertical: 10,
+              horizontal: 16,
+            ),
+            hintText: hint,
+            hintStyle: Theme.of(context)
+                .textTheme
+                .headline6
+                .copyWith(color: AppColorsLight.secondary2),
+            suffix: Container(
+              height: 20,
+              width: 20,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: Colors.black, //remove this when you add image.
+              ),
+              child: IconButton(
+                padding: EdgeInsets.all(2),
+                icon: SvgPicture.asset(
+                  AppIcons.iconClose,
+                  color: Colors.white,
+                ),
+                onPressed: () {
+                  controller.clear();
+                  _valuesAdd[nameField] = '';
+                  print('press close');
+                },
+              ),
+            ),
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.all(
+                Radius.circular(8.0),
+              ),
+            ),
+            enabledBorder: OutlineInputBorder(
+              borderSide: BorderSide(
+                color: Theme.of(context).brightness == Brightness.dark
+                    ? AppColorsDark.borderGreen
+                    : AppColorsLight.borderGreen,
+              ),
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderSide: BorderSide(
+                color: Theme.of(context).brightness == Brightness.dark
+                    ? AppColorsDark.borderGreen
+                    : AppColorsLight.borderGreen,
+                width: 2,
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  // checkAddValues проверяет все ли поля заполнены
+  void checkAddValues() {
+    _check = false;
+    if (_valuesAdd['name'] != '' &&
+        _valuesAdd['lat'] != '' &&
+        _valuesAdd['lon'] != '' &&
+        _valuesAdd['details'] != '') {
+      _check = true;
+    }
+    setState(() {});
+  }
+
+  /// addNewSight добавляет новое место [Sight] в массив моковых даных[mocks]
+  void addNewSight() {
+    var newSight = Sight(
+      name: _valuesAdd['name'],
+      lat: double.parse(_valuesAdd['lat'].toString()),
+      lon: double.parse(_valuesAdd['lon'].toString()),
+      url: 'url',
+      details: _valuesAdd['details'],
+      type: '',
+    );
+
+    mocks.add(newSight);
+    print(mocks);
+
+    nameController.clear();
+    descriptionController.clear();
+    latController.clear();
+    lonController.clear();
+
+    _check = false;
+
+    setState(() {});
+  }
 }
 
+/// SelectAddCategories - открывает screen AddCategory
+/// для выбора категории нового места
 class SelectAddCategories extends StatefulWidget {
   @override
   _SelectAddCategoriesState createState() => _SelectAddCategoriesState();
@@ -166,85 +327,4 @@ class _SelectAddCategoriesState extends State<SelectAddCategories> {
       ],
     );
   }
-}
-
-Widget _addField({
-  TextEditingController controller,
-  FocusNode currentFocus,
-  FocusNode nextFocus,
-  String hint,
-  bool multipleLines,
-  bool lastField = false,
-  BuildContext context,
-  String title,
-  bool numericField = false,
-}) {
-  assert(controller != null);
-  assert(currentFocus != null);
-  assert(hint != null);
-  assert(multipleLines != null);
-  assert(context != null);
-  assert(title != null);
-  return Column(
-    crossAxisAlignment: CrossAxisAlignment.start,
-    children: [
-      Container(
-        padding: EdgeInsets.only(
-          top: 24.0,
-          bottom: 12,
-        ),
-        child: Text(title.toUpperCase()),
-      ),
-      TextField(
-        minLines: multipleLines ? 2 : null,
-        maxLines: multipleLines ? 2 : null,
-        controller: controller,
-        focusNode: currentFocus,
-        keyboardType: numericField ? TextInputType.number : null,
-        textInputAction: TextInputAction.go,
-        onEditingComplete: () {
-          lastField
-              ? FocusScope.of(context).unfocus()
-              : FocusScope.of(context).requestFocus(nextFocus);
-        },
-        decoration: InputDecoration(
-          contentPadding: EdgeInsets.symmetric(
-            vertical: 10,
-            horizontal: 16,
-          ),
-          hintText: hint,
-          hintStyle: Theme.of(context)
-              .textTheme
-              .headline6
-              .copyWith(color: AppColorsLight.secondary2),
-          border: OutlineInputBorder(
-            borderRadius: BorderRadius.all(
-              Radius.circular(8.0),
-            ),
-          ),
-          enabledBorder: OutlineInputBorder(
-            borderSide: BorderSide(
-              color: Theme.of(context).brightness == Brightness.dark
-                  ? AppColorsDark.inactiveBlack
-                  : AppColorsLight.inactiveBlack,
-            ),
-          ),
-          focusedBorder: OutlineInputBorder(
-            borderSide: BorderSide(
-              color: Theme.of(context).brightness == Brightness.dark
-                  ? AppColorsDark.borderGreen
-                  : AppColorsLight.borderGreen,
-            ),
-          ),
-          disabledBorder: OutlineInputBorder(
-            borderSide: BorderSide(
-              color: Theme.of(context).brightness == Brightness.dark
-                  ? AppColorsDark.borderGreen
-                  : AppColorsLight.borderGreen,
-            ),
-          ),
-        ),
-      ),
-    ],
-  );
 }
