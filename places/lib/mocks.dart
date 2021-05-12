669a4749b2405d1c70b7e081a21a46386ed270ee
nikolaiKopylov <nikolai.kopylov89@gmail.com>
2021-05-12 23:30:57 +0300

    fix(mocks): добавлены точки в мое локации для фильтрации и поиска

--- places/lib/mocks.dart
+++ places/lib/mocks.dart
@@ -1,6 +1,7 @@
 import 'package:places/domain/sight.dart';
 
 import 'domain/category.dart';
+import 'domain/point.dart';
 import 'ui/constants.dart';
 
 /// Моковые даные для Sight
@@ -13,7 +14,7 @@ final List<Sight> mocks = [
         'https://34travel.me/media/upload/images/2020/OCTOBER/34dstpr-new/296A1215.jpg',
     details:
         'Мирский замок, замково-парковый комплекс «Мир» — оборонительное укрепление и резиденция в городском посёлке Мир Кореличского района Гродненской области Белоруссии. Памятник архитектуры, внесён в список Всемирного наследия ЮНЕСКО',
-    type: 'замок',
+    type: filterList[2],
   ),
   Sight(
     name: 'Брестская крепость',
@@ -23,7 +24,7 @@ final List<Sight> mocks = [
         'https://34travel.me/media/upload/images/2020/SEPTEMBER/brest-new/296A5725.jpg',
     details:
         'Мощное оборонительное сооружение и масштабный музей советской героической монументалистики под открытым небом. Сама крепость была построена в XIX веке, и история у нее впечатляющая. ',
-    type: 'крепость',
+    type: filterList[3],
   ),
   Sight(
     name: 'Беловежская пуща',
@@ -33,8 +34,28 @@ final List<Sight> mocks = [
         'https://34travel.me/media/upload/images/2017/april/34_bel_dostoprim/new/IMG_6978.jpg',
     details:
         'Беларусский повод для гордости – самый большой и старый лесной массив Европы, охраняется ЮНЕСКО. Да, здесь в дикой природе водятся зубры. Да, на них можно даже охотится (если хватит совести). Да, это здесь подписали соглашение о развале СССР. ',
-    type: 'заповедик',
-  )
+    type: filterList[3],
+  ),
+  Sight(
+    name: 'Терриконы',
+    lat: 52.7822910,
+    lon: 27.5372246,
+    url:
+        'https://34travel.me/media/upload/images/2020/MAY/marshrut-luban/IMG_7526.jpg',
+    details:
+        'Солигорские терриконы вызывают восторг с любого расстояния: понимаешь, что здесь не может быть таких высоких объектов, но тихо радуешься. Горы все-таки есть. Их высота местами превышает 120 метров, белые верхушки словно присыпаны снегом, а внизу и вовсе какой-то иранский пейзаж. ',
+    type: filterList[2],
+  ),
+  Sight(
+    name: 'Парк Четырех Стихий',
+    lat: 52.7822910,
+    lon: 27.5372246,
+    url:
+        'https://vetliva.ru/upload/adwex.minified/webp/f09/90/f093cdf0fe8f115047fbc7f46586e021.webp',
+    details:
+        'Название парка отсылает к основной идее его создателя. Стихии, представленные разными архитектурными воплощениями, призваны символизировать единство природы и красоту всех ее состояний.',
+    type: filterList[3],
+  ),
 ];
 
 /// filterList - данные о фильтрах(категориях, для фильтрации)
@@ -70,3 +91,5 @@ const List<Category> filterList = const <Category>[
     icon: AppIcons.iconCafe,
   ),
 ];
+
+final currentPoint = Point(lat: 52.78472, lon: 27.53500);
